709039d46fe3a422009add9074573243b8d8710a
niranjani-sparks <niranjanibalaji08122004@gmail.com>
2026-05-08 12:46:14 +0530

    use connection pool

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,22 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQLHOST || process.env.DB_HOST,
   user: process.env.MYSQLUSER || process.env.DB_USER,
   password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
   database: process.env.MYSQLDATABASE || process.env.DB_NAME,
-  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error('DB connection failed:', err);
     return;
   }
   console.log('MySQL connected!');
+  connection.release();
 });
 
 module.exports = db;
